5f10c2e48540cde2fbbf1d52d00055f16f6992d0
Grant Moore <gmoore@Grants-MacBook-Pro.local>
2025-07-29 19:45:23 -0400

    update build scripts

--- scripts/linux/provisioner.sh
+++ scripts/linux/provisioner.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 set -e # Exit immediately if a command fails
 
-# 1. Apply all system updates
+# 1. Remove the problematic command-not-found utility
 export DEBIAN_FRONTEND=noninteractive
+sudo apt-get remove -y command-not-found
+sudo apt-get autoremove -y
+
+# 2. Apply all system updates
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# 2. Install Git
+# 3. Install Git
 sudo apt-get install -y git
 
-# 3. Install Docker Engine
+# 4. Install Docker Engine
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-# 4. Install the SSM Agent
+# 5. Install the SSM Agent
 sudo snap wait system seed.loaded
 sudo snap install amazon-ssm-agent --classic
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
+sudo systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
+sudo systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service
